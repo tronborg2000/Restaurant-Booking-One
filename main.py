3718a21bbc3485c7db986c9711f1580bba9d082f
tronborg2000 <colinweldon1980@gmail.com>
2023-03-16 12:16:56 +0000

    add booking html

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -6,6 +6,22 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/booking')
+def booking():
+    return render_template('booking.html')
+
+@app.route('/submit-booking', methods=['POST'])
+def submit_booking():
+    name = request.form['name']
+    email = request.form['email']
+    phone = request.form['phone']
+    date = request.form['date']
+    time = request.form['time']
+    guests = request.form['guests']
+    
+
+
+    return redirect(url_for('booking'))
+
 if __name__ == '__main__':
     app.run(debug=True)
-
