d158f043034efc1fbe947a3a510bf6b9fdc1d059
tronborg2000 <colinweldon1980@gmail.com>
2023-03-19 17:43:32 +0000

    update python thank you message

--- main.py
+++ main.py
@@ -29,29 +29,32 @@ def is_available(date, time):
 def home():
     return render_template('index.html')
 
-@app.route('/booking')
+@app.route('/booking', methods=['GET', 'POST'])
 def booking():
-    return render_template('booking.html')
+    success = request.args.get('success')
+    error = None
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
 
-@app.route('/submit-booking', methods=['POST'])
-def submit_booking():
-    name = request.form['name']
-    email = request.form['email']
-    phone = request.form['phone']
+        try:
+            date = datetime.strptime(request.form['date'], '%Y-%m-%d').date()
+            time = datetime.strptime(request.form['time'], '%H:%M').time()
+        except ValueError:
+            return render_template('booking.html', error="Invalid date or time format. Please try again.")
 
-    try:
-        date = datetime.strptime(request.form['date'], '%Y-%m-%d').date()
-        time = datetime.strptime(request.form['time'], '%H:%M').time()
-    except ValueError:
-        return render_template('booking.html', error="Invalid date or time format. Please try again.")
+        guests = int(request.form['guests'])
 
-    guests = int(request.form['guests'])
+        if is_available(date, time):
+            save_booking(name, email, phone, date, time, guests)
+            success = "Thank you for your booking!"
+        else:
+            error = "The selected date and time are not available."
 
-    if is_available(date, time):
-        save_booking(name, email, phone, date, time, guests)
-        return redirect(url_for('booking'))
-    else:
-        return render_template('booking.html', error="The selected date and time are not available.")
+        print("Success:", success, "Error:", error)  # Add this line
+
+    return render_template('booking.html', success=success, error=error)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
